a7991266379bb1885d0d8780995be28e71b0efd4
anasgaya <anaasraheel.123@gmail.com>
2024-08-15 14:33:56 -0700

    15 question completed

---
+++ app.js
@@ -0,0 +1,105 @@
+//question no 1
+
+// let firstName = prompt("enter your first name");
+// let lastName = prompt("enter your last name");
+// let fullName = firstName + ' ' + lastName; 
+
+// alert("wellcome " + fullName);
+
+
+// question no 2
+
+// let model = prompt("enter mobile model");
+
+// console.log(`my favourite phone is : ${model}` );
+// console.log(`lenght of string ${model.length}`);
+
+
+// question no 3
+
+// let country = "pakistani";
+
+// console.log(`string ${country}` );
+// console.log(`index of 'n' ${country.indexOf('n')}`);
+
+
+// question no 4
+
+// let hello = "hello world";
+
+// console.log(`string ${hello}`);
+// console.log(`last index of 'l' ${hello.lastIndexOf('l')}`);
+
+// question no 5
+
+// let country = "pakistani";
+
+// console.log(`string : ${country}`);
+// console.log(`character at index '3' ${country.charAt('3')}`);
+
+// question no 6
+
+//  let nameFirst = prompt("enter your first name");
+//  let nameLast = prompt("enter your last name");
+//  let nameFull = nameFirst.concat(" ",nameLast);
+//  console.log(nameFull);
+
+// question no 7
+
+// let city = "hyderabad";
+
+
+// console.log(`city ${city}`);
+
+// console.log(`after replacement ${ city.replace("hyder","islam")}`);
+
+
+// question no 8
+
+// let message = "ali and sami are best friend.they play cirket and football together.";
+
+// console.log(`${message.replaceAll("and","&")}`)
+
+// question no 9
+
+// let string = "472"
+// let num = Number(string)
+// console.log(`value : ${string}`);
+// console.log(`type of ${typeof(string)}`);
+// console.log(`value : ${num}`);
+// console.log(`type : ${typeof(num)}`);
+
+
+//  question no 10
+
+ var email = prompt("enter url");
+ console.log(`url : ${email}`);
+ console.log(`domain ${email.slice(4)}`);
+
+// question no 11
+
+// let biscuit = "peanuts";
+// console.log(`user input : ${biscuit}`);
+// console.log(`uppercase : ${biscuit.toUpperCase()}`);
+
+// question no 12
+
+// let sim = prompt("enter sim")
+// console.log(`user input : ${sim}`);
+// console.log(`lower case : ${sim.toLowerCase()}`);
+
+
+// question no 13 
+
+// let num = 35.36
+// let string = num.toString();
+// console.log(`number : ${num}`);
+// console.log(`result : ${ typeof(string)}`);
+// console.log(string.replace(".",""));
+
+// question no 14
+
+// let num1 = "3";
+// let num2 = "3";
+// let x = num1 + num2;
+// console.log(`a + b = ${x}`);
